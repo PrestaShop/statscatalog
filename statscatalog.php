2f00e89533c8da8ffb492797111eaa080fdda86e
Michel ANTOINE <michel.br.antoine@gmail.com>
2017-01-09 11:07:06 +0100

    CSFIX
    (cherry picked from commit 5aac78bd152828569b233e9168af29ad0c6e2a07)

--- statscatalog.php
+++ statscatalog.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2016 PrestaShop
+* 2007-2015 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,42 +19,42 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
+*  @copyright  2007-2015 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
-class StatsCatalog extends Module
+class statscatalog extends Module
 {
-	private $join = '';
-	private $where = '';
+    private $join = '';
+    private $where = '';
 
-	public function __construct()
-	{
-		$this->name = 'statscatalog';
-		$this->tab = 'analytics_stats';
-		$this->version = '1.4.0';
-		$this->author = 'PrestaShop';
-		$this->need_instance = 0;
+    public function __construct()
+    {
+        $this->name = 'statscatalog';
+        $this->tab = 'analytics_stats';
+        $this->version = '1.3.0';
+        $this->author = 'PrestaShop';
+        $this->need_instance = 0;
 
-		parent::__construct();
+        parent::__construct();
 
-		$this->displayName = $this->l('Catalog statistics');
-		$this->description = $this->l('Adds a tab containing general statistics about your catalog to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7.0.99');
-	}
+        $this->displayName = $this->l('Catalog statistics');
+        $this->description = $this->l('Adds a tab containing general statistics about your catalog to the Stats dashboard.');
+    }
 
-	public function install()
-	{
-		return (parent::install() && $this->registerHook('AdminStatsModules'));
-	}
+    public function install()
+    {
+        return (parent::install() && $this->registerHook('AdminStatsModules'));
+    }
 
-	public function getQuery1()
-	{
-		$sql = 'SELECT COUNT(DISTINCT p.`id_product`) AS total, SUM(product_shop.`price`) / COUNT(product_shop.`price`) AS average_price, COUNT(DISTINCT i.`id_image`) AS images
+    public function getQuery1()
+    {
+        $sql = 'SELECT COUNT(DISTINCT p.`id_product`) AS total, SUM(product_shop.`price`) / COUNT(product_shop.`price`) AS average_price, COUNT(DISTINCT i.`id_image`) AS images
 				FROM `'._DB_PREFIX_.'product` p
 				'.Shop::addSqlAssociation('product', 'p').'
 				LEFT JOIN `'._DB_PREFIX_.'image` i ON i.`id_product` = p.`id_product`
@@ -62,12 +62,12 @@ class StatsCatalog extends Module
 				WHERE product_shop.`active` = 1
 					'.$this->where;
 
-		return DB::getInstance(_PS_USE_SQL_SLAVE_)->getRow($sql);
-	}
+        return DB::getInstance(_PS_USE_SQL_SLAVE_)->getRow($sql);
+    }
 
-	public function getTotalPageViewed()
-	{
-		return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+    public function getTotalPageViewed()
+    {
+        return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
 		SELECT SUM(pv.`counter`)
 		FROM `'._DB_PREFIX_.'product` p
 		'.Shop::addSqlAssociation('product', 'p').'
@@ -77,11 +77,11 @@ class StatsCatalog extends Module
 		'.$this->join.'
 		WHERE product_shop.`active` = 1
 		'.$this->where);
-	}
+    }
 
-	public function getTotalProductViewed()
-	{
-		return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+    public function getTotalProductViewed()
+    {
+        return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
 		SELECT COUNT(DISTINCT pa.`id_object`)
 		FROM `'._DB_PREFIX_.'page_viewed` pv
 		LEFT JOIN `'._DB_PREFIX_.'page` pa ON pv.`id_page` = pa.`id_page`
@@ -92,11 +92,11 @@ class StatsCatalog extends Module
 		WHERE pt.`name` IN ("product.php", "product")
 		AND product_shop.`active` = 1
 		'.$this->where);
-	}
+    }
 
-	public function getTotalBought()
-	{
-		return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+    public function getTotalBought()
+    {
+        return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
 		SELECT SUM(od.`product_quantity`)
 		FROM `'._DB_PREFIX_.'orders` o
 		LEFT JOIN `'._DB_PREFIX_.'order_detail` od ON o.`id_order` = od.`id_order`
@@ -104,11 +104,11 @@ class StatsCatalog extends Module
 		'.$this->join.'
 		WHERE o.valid = 1
 		'.$this->where);
-	}
+    }
 
-	public function getProductsNB($id_lang)
-	{
-		$sql = 'SELECT p.`id_product`
+    public function getProductsNB($id_lang)
+    {
+        $sql = 'SELECT p.`id_product`
 				FROM `'._DB_PREFIX_.'orders` o
 				LEFT JOIN `'._DB_PREFIX_.'order_detail` od ON o.`id_order` = od.`id_order`
 				LEFT JOIN `'._DB_PREFIX_.'product` p ON p.`id_product` = od.`product_id`
@@ -118,13 +118,14 @@ class StatsCatalog extends Module
 					'.$this->where.'
 					AND product_shop.`active` = 1
 				GROUP BY p.`id_product`';
-		$precalc = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+        $precalc = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
 
-		$precalc2 = array();
-		foreach ($precalc as $array)
-			$precalc2[] = (int)$array['id_product'];
+        $precalc2 = array();
+        foreach ($precalc as $array) {
+            $precalc2[] = (int)$array['id_product'];
+        }
 
-		$sql = 'SELECT p.id_product, pl.name, pl.link_rewrite
+        $sql = 'SELECT p.id_product, pl.name, pl.link_rewrite
 				FROM `'._DB_PREFIX_.'product` p
 				'.Shop::addSqlAssociation('product', 'p').'
 				LEFT JOIN `'._DB_PREFIX_.'product_lang` pl
@@ -133,45 +134,45 @@ class StatsCatalog extends Module
 				WHERE product_shop.`active` = 1
 					'.(count($precalc2) ? 'AND p.`id_product` NOT IN ('.implode(',', $precalc2).')' : '').'
 					'.$this->where;
-		$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
 
-		return array('total' => Db::getInstance(_PS_USE_SQL_SLAVE_)->NumRows(), 'result' => $result);
-	}
+        return array('total' => Db::getInstance(_PS_USE_SQL_SLAVE_)->NumRows(), 'result' => $result);
+    }
 
-	public function hookAdminStatsModules($params)
-	{
-		$categories = Category::getCategories($this->context->language->id, true, false);
-		$product_token = Tools::getAdminToken('AdminProducts'.(int)Tab::getIdFromClassName('AdminProducts').(int)$this->context->employee->id);
-		$irow = 0;
+    public function hookAdminStatsModules($params)
+    {
+        $categories = Category::getCategories($this->context->language->id, true, false);
+        $product_token = Tools::getAdminToken('AdminProducts'.(int)Tab::getIdFromClassName('AdminProducts').(int)$this->context->employee->id);
+        $irow = 0;
 
-		if ($id_category = (int)Tools::getValue('id_category'))
-		{
-			$this->join = ' LEFT JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_product` = p.`id_product`)';
-			$this->where = ' AND cp.`id_category` = '.$id_category;
-		}
+        if ($id_category = (int)Tools::getValue('id_category')) {
+            $this->join = ' LEFT JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_product` = p.`id_product`)';
+            $this->where = ' AND cp.`id_category` = '.$id_category;
+        }
 
-		$result1 = $this->getQuery1(true);
-		$total = $result1['total'];
-		$average_price = $result1['average_price'];
-		$total_pictures = $result1['images'];
-		$average_pictures = $total ? $total_pictures / $total : 0;
+        $result1 = $this->getQuery1(true);
+        $total = $result1['total'];
+        $average_price = $result1['average_price'];
+        $total_pictures = $result1['images'];
+        $average_pictures = $total ? $total_pictures / $total : 0;
 
-		$never_bought = $this->getProductsNB($this->context->language->id);
-		$total_nb = $never_bought['total'];
-		$products_nb = $never_bought['result'];
+        $never_bought = $this->getProductsNB($this->context->language->id);
+        $total_nb = $never_bought['total'];
+        $products_nb = $never_bought['result'];
 
-		$total_bought = $this->getTotalBought();
-		$average_purchase = $total ? ($total_bought / $total) : 0;
+        $total_bought = $this->getTotalBought();
+        $average_purchase = $total ? ($total_bought / $total) : 0;
 
-		$total_page_viewed = $this->getTotalPageViewed();
-		$average_viewed = $total ? ($total_page_viewed / $total) : 0;
-		$conversion = number_format((float)($total_page_viewed ? ($total_bought / $total_page_viewed) : 0), 2, '.', '');
-		if ($conversion_reverse = number_format((float)($total_bought ? ($total_page_viewed / $total_bought) : 0), 2, '.', ''))
-			$conversion .= sprintf($this->l('(1 purchase / %d visits)'), $conversion_reverse);
+        $total_page_viewed = $this->getTotalPageViewed();
+        $average_viewed = $total ? ($total_page_viewed / $total) : 0;
+        $conversion = number_format((float)($total_page_viewed ? ($total_bought / $total_page_viewed) : 0), 2, '.', '');
+        if ($conversion_reverse = number_format((float)($total_bought ? ($total_page_viewed / $total_bought) : 0), 2, '.', '')) {
+            $conversion .= sprintf($this->l('(1 purchase / %d visits)'), $conversion_reverse);
+        }
 
-		$total_nv = $total - $this->getTotalProductViewed();
+        $total_nv = $total - $this->getTotalProductViewed();
 
-		$html = '
+        $html = '
 		<script type="text/javascript">$(\'#calendar\').slideToggle();</script>
 			<div class="panel-heading">
 				'.$this->displayName.'
@@ -184,11 +185,12 @@ class StatsCatalog extends Module
 					<div class="col-lg-6">
 						<select name="id_category" onchange="$(\'#categoriesForm\').submit();">
 							<option value="0">'.$this->l('All').'</option>';
-		foreach ($categories as $category)
-			$html .= '<option value="'.$category['id_category'].'"'.($id_category == $category['id_category'] ? ' selected="selected"' : '').'>'.
-				$category['name'].'
+        foreach ($categories as $category) {
+            $html .= '<option value="'.$category['id_category'].'"'.($id_category == $category['id_category'] ? ' selected="selected"' : '').'>'.
+                $category['name'].'
 							</option>';
-		$html .= '
+        }
+        $html .= '
 						</select>
 					</div>
 				</div>
@@ -212,9 +214,8 @@ class StatsCatalog extends Module
 				</p>
 			</div>';
 
-		if (count($products_nb) && count($products_nb) < 50)
-		{
-			$html .= '
+        if (count($products_nb) && count($products_nb) < 50) {
+            $html .= '
 				<div class="panel-heading">'.$this->l('Products never purchased').'</div>
 				<table class="table">
 					<thead>
@@ -225,15 +226,14 @@ class StatsCatalog extends Module
 						</tr>
 					</thead>
 					<tbody>';
-			foreach ($products_nb as $product) {
-				$urlParams = array('id_product' => $product['id_product'], 'updateproduct' => '1');
-				$html .= '
+            foreach ($products_nb as $product) {
+                $html .= '
 					<tr'.($irow++ % 2 ? ' class="alt_row"' : '').'>
 						<td>'.$product['id_product'].'</td>
 						<td>'.$product['name'].'</td>
 						<td class="left">
 							<div class="btn-group btn-group-action">
-								<a class="btn btn-default" href="' . Tools::safeOutput(preg_replace("/\\?.*$/", '?tab=AdminProducts&id_product=' . $product['id_product'] . '&updateproduct&token=' . $product_token, $this->context->link->getAdminLink('AdminProducts', true, $urlParams))) . '" target="_blank">
+								<a class="btn btn-default" href="'.Tools::safeOutput('index.php?tab=AdminProducts&id_product='.$product['id_product'].'&addproduct&token='.$product_token).'" target="_blank">
 									<i class="icon-edit"></i> '.$this->l('Edit').'
 								</a>
 								<button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button">
@@ -249,17 +249,17 @@ class StatsCatalog extends Module
 							</div>
 						</td>
 					</tr>';
-			}
-			$html .= '
+            }
+            $html .= '
 					</tbody>
 				</table>';
-		}
+        }
 
-		return $html;
-	}
+        return $html;
+    }
 
-	private function returnLine($label, $data)
-	{
-		return $label.$data;
-	}
+    private function returnLine($label, $data)
+    {
+        return $label.$data;
+    }
 }
