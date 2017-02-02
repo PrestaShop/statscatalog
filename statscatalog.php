be6be71c703900dda0182e56c9c6658ab85f215b
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-02 09:39:32 +0100

    // Version updated to v2.0.1

--- statscatalog.php
+++ statscatalog.php
@@ -37,7 +37,7 @@ class statscatalog extends Module
     {
         $this->name = 'statscatalog';
         $this->tab = 'analytics_stats';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
