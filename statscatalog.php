d821d9e5a6c9da4dc453336b1ab6451306f5428d
Jonathan Danse <j.danse@prestaedit.com>
2021-12-30 10:59:57 +0100

    Next version is 2.0.2

--- statscatalog.php
+++ statscatalog.php
@@ -42,7 +42,7 @@ class statscatalog extends Module
     {
         $this->name = 'statscatalog';
         $this->tab = 'analytics_stats';
-        $this->version = '2.0.3';
+        $this->version = '2.0.2';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
