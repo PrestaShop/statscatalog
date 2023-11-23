9594ad39c076f60ffe259ab10f83bfc8a8cb577f
Daniel Hlaváček <daniel.hlavacek@hotmail.cz>
2023-11-23 14:22:48 +0100

    Update statscatalog.php

--- statscatalog.php
+++ statscatalog.php
@@ -42,7 +42,7 @@ class statscatalog extends Module
     {
         $this->name = 'statscatalog';
         $this->tab = 'administration';
-        $this->version = '2.0.3';
+        $this->version = '2.0.4';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
